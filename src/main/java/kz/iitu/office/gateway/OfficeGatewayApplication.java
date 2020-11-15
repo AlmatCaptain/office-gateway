0b6e0c2b114cdc028c7d8d9fe0eeafae2a46f45f
almatzhagiparov <almatzhagiparov@gmail.com>
2020-11-15 23:29:16 +0600

    zuul commit

---
+++ src/main/java/kz/iitu/office/gateway/OfficeGatewayApplication.java
@@ -0,0 +1,17 @@
+package kz.iitu.office.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class OfficeGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfficeGatewayApplication.class, args);
+    }
+
+}
